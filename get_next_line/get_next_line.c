deafc8c0a47364fc0562b1fbcc6babe602c6cbfc
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2022-11-22 14:59:56 +0000

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: luaraujo <luaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 14:17:17 by luaraujo          #+#    #+#             */
-/*   Updated: 2022/11/21 18:10:05 by luaraujo         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:57:43 by luaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,22 @@ static char	*get_line(char *str)
 	int		i;
 
 	i = 0;
-	if (!str[i])
+	if (!str)
 		return (NULL);
 	while (str[i] != '\n' && str[i] != '\0')
 		i++;
-	line = ft_calloc((i + 1), sizeof(char));
+	line = ft_calloc((i + 2), sizeof(char));
 	i = 0;
 	while (str[i] != '\n' && str[i] != '\0')
 	{
 		line[i] = str[i];
+		i++;
+	}
+	if (str[0] == '\0')
+		return (NULL);
+	if (str[i] == '\n')
+	{
+		line[i] = '\n';
 		i++;
 	}
 	line[i] = '\0';
@@ -64,6 +71,8 @@ static char	*read_file(int fd, char *str)
 	char	*aux;
 	ssize_t	return_read;
 
+	if (!str)
+		str = ft_calloc(1, 1);
 	aux = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	return_read = read(fd, aux, BUFFER_SIZE);
 	str = ft_strjoin(str, aux);
@@ -71,14 +80,16 @@ static char	*read_file(int fd, char *str)
 	if (return_read == -1)
 		return (NULL);
 	if (return_read == 0 && str == NULL)
+	{
+		free(str);
 		return (NULL);
+	}
 	if (ft_strchr(str, '\n') == NULL && return_read != 0)
 	{
 		aux = ft_calloc(1, 1);
 		return (ft_strjoin(str, read_file(fd, aux)));
 	}
-	else
-		return (str);
+	return (str);
 }
 
 char	*get_next_line(int fd)
@@ -89,14 +100,12 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	if (!str)
-		str = ft_calloc(1, 1);
 	str = read_file(fd, str);
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 	line = get_line(str);
 	i = 0;
-	while (str[i] != '\n')
+	while (str[i] != '\n' && str[i])
 		i++;
 	str = delsubstr(str, 0, i);
 	return (line);
